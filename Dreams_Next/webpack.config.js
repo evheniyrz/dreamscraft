0d9f18404ac2d76820ca8da363f41ee22b1d87f3
evgenjnr <evgeniyr7@gmail.com>
2018-08-08 11:30:13 +0300

    file formatting

--- Dreams_Next/webpack.config.js
+++ Dreams_Next/webpack.config.js
@@ -8,53 +8,53 @@ const conf = {
 		filename: 'main.js',
 		// publicPath: 'dist/'
 	},
-	module:{
-		rules:[
-		   {
-			test: /\.js$/,
-			loader: 'babel-loader',
-			//exlude: 'node_module'
-	       },
-		{
-		test: /\.css$/,
-			use: ExtractTextPlugin.extract({
-			fallback: "style-loader",
-			use: "css-loader"
-			})
-		  }
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				//exlude: 'node_module'
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader"
+				})
+			}
 		]
 	},
-	devServer : {
+	devServer: {
 		contentBase: './src',
 		watchContentBase: true,
 		overlay: true
 	},
 	plugins: [
-			new ExtractTextPlugin('css/style.css'),
-			new HtmlWebpackPlugin({ 
-				template: './src/index.html',
-      filename: './index.html',
-      hash: false,
-      inject: true,
-      compile: true,
-      favicon: false,
-      minify: false,
-      cache: true,
-      showErrors: true,
-      //chunks: 'all',
-      //excludeChunks: [],
-      title: 'App',
-      xhtml: true
-			})
+		new ExtractTextPlugin('css/style.css'),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+			hash: false,
+			inject: true,
+			compile: true,
+			favicon: false,
+			minify: false,
+			cache: true,
+			showErrors: true,
+			//chunks: 'all',
+			//excludeChunks: [],
+			title: 'App',
+			xhtml: true
+		})
 
-		]
-	};
+	]
+};
 
 module.exports = (env, options) => {
 	let produc = options.mode === 'productions';
-			conf.devtool = produc 
-			? false
-			: 'eval-source';
-			return conf;
+	conf.devtool = produc
+		? false
+		: 'eval-source';
+	return conf;
 }
 /**/
