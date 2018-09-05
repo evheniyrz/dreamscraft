637cd226e3f15ec96c16f510b4881708201c3d5f
evgenjnr <evgeniyr7@gmail.com>
2018-09-05 10:53:36 +0300

    refactoring js code

--- Dreams_DOM/script/script.js
+++ Dreams_DOM/script/script.js
@@ -1,34 +1,38 @@
 window.addEventListener("DOMContentLoaded", beauty);
-	let progressBar, contentText, viewPort, displayCount, input, countMonitor;
-function beauty(){
-	 progressBar    = document.querySelector(".progressBar > .progressIndicator");
-	 contentText    = document.querySelector(".container");
-	 viewPort       = document.querySelector(".wrapper");
-	 displayCount   = document.querySelector(".monitor");
-	 input          = document.querySelector("input[type='color']");
-	 countMonitor   = document.querySelector(".countMonitor");
-	 displayCount.innerHTML = contentText.scrollTop + " px";
-	 
+
+function beauty() {
+	const progressBar = document.getElementById("progressIndicator");
+	const contentText = document.getElementById("container");
+	const viewPort = document.getElementById("wrapper");
+	const displayCount = document.getElementById("monitor");
+	const input = document.querySelector("input[type='color']");
+	const countMonitor = document.getElementById("countMonitor");
+
+	displayCount.innerHTML = contentText.scrollTop + " px";
+
 	beast();
+
 	window.addEventListener("resize", beast);
+	input.addEventListener("change", selectColor);
 
-	input.addEventListener("input", ()=>{
-				
-				let color                 = input.value;
-				viewPort.style.background = color;
-				displayCount.style.color  = color;
-			});
+	function selectColor() {
+		let color = input.value;
+		viewPort.style.background = color;
+		displayCount.style.color = color;
+	}
+
+	function beast() {
+		let viewportHeight = viewPort.clientHeight;
+		let contentHeight = contentText.scrollHeight;
+		let differ = (contentHeight - viewportHeight) / 100;
+
+		contentText.addEventListener("scroll", () => {
+			progressBar.style.width = contentText.scrollTop / differ + "%";
+			displayCount.innerHTML = contentText.scrollTop + " px";
+			countMonitor.innerHTML = Math.round(contentText.scrollTop / differ) + "%";
+		});
+
+	}
+	
 }
 
-function beast(){
-	let viewportHeight = viewPort.clientHeight;
-	let contentHeight  = contentText.scrollHeight;
-	let differ         = (contentHeight - viewportHeight) / 100;
-
-	contentText.addEventListener("scroll", ()=>{
-		progressBar.style.width = contentText.scrollTop / differ + "%";
-		displayCount.innerHTML = contentText.scrollTop + " px";
-		countMonitor.innerHTML = Math.round(contentText.scrollTop / differ) + "%";
-	});
-
-}
